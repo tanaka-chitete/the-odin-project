e180630ba8d8c5d8fee70ab4f9c2a402b87ac25f
Tanaka Chitete <tanaka.chitete@gmail.com>
2024-08-13 15:51:24 +1000

    Display added book

--- full-stack-javascript/javascript/organizing-your-javascript-code/library/index.js
+++ full-stack-javascript/javascript/organizing-your-javascript-code/library/index.js
@@ -17,10 +17,11 @@ function Book(title, author, numPages, read) {
 const library = new Array();
 
 function addBookToLibrary(title, author, numPages, read) {
-  library.push(new Book(title, author, numPages, read));
-  console.dir(library);
-}
+  const book = new Book(title, author, numPages, read);
+  library.push(book);
 
+  return book;
+}
 
 const addBookDialog = document.querySelector(".add-book-dialog");
 const addBookButton = document.querySelector(".add-book-button");
@@ -31,12 +32,22 @@ const addBookForm = document.querySelector(".add-book-dialog__form");
 addBookForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  addBookToLibrary(addBookForm.elements["title"].value, 
-                   addBookForm.elements["author"].value, 
-                   Number(addBookForm.elements["numPages"].value),
-                   String(addBookForm.elements["read"].value) === "true"   
-                  )
+  const bookObject = addBookToLibrary(
+    addBookForm.elements["title"].value, 
+    addBookForm.elements["author"].value, 
+    Number(addBookForm.elements["numPages"].value),
+    String(addBookForm.elements["read"].value) === "true"   
+  )
 
+  const library = document.querySelector(".library");
+  const bookElement = `
+    <li data-index="${library.childElementCount} class="library__book">
+      <p class="library__book-info">${bookObject.info()}</p>
+      <button class="library__book-remove-book-button">Remove</button>
+    </li>
+  `;
+  library.innerHTML += bookElement;
+  
   addBookForm.reset();
 
   addBookDialog.close();
