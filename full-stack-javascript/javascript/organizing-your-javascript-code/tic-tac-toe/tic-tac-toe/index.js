7be705d577adeebcaf7fb1253808940221397a8b
Tanaka Chitete <tanaka.chitete@gmail.com>
2024-08-15 10:38:13 +1000

    Move logic to determine winner to controller

--- full-stack-javascript/javascript/organizing-your-javascript-code/tic-tac-toe/tic-tac-toe/index.js
+++ full-stack-javascript/javascript/organizing-your-javascript-code/tic-tac-toe/tic-tac-toe/index.js
@@ -24,6 +24,8 @@ const board = (function() {
 
   const getBoard = () => board;
 
+  const getCell = (row, column) => getBoard()[row][column];
+
   const drawSymbol = (row, column, symbol) => {
     // A symbol has already been drawn in this cell
     if (board[row][column]) {
@@ -35,34 +37,10 @@ const board = (function() {
     return true;
   };
 
-  const determineWinner = () => {
-    // checkRow
-    // checkColumn
-    // checkDiagonal
-    function checkRow(row) {
-      return (
-        getBoard()[row][0].getSymbol() === getBoard()[row][1].getSymbol() &&
-        getBoard()[row][1].getSymbol() === getBoard()[row][2].getSymbol()
-      );
-    }
-
-    function checkColumn(column) {
-      return (
-        getBoard()[0][column].getSymbol() === getBoard()[1][column].getSymbol() &&
-        getBoard()[1][column].getSymbol() === getBoard()[2][column].getSymbol()
-      );
-    }
-
-    function checkDiagonal(startRow, step) {
-      return (
-        getBoard()[startRow][0].getSymbol() === getBoard()[startRow + step][1].getSymbol() &&
-        getBoard()[startRow + step][1].getSymbol() === getBoard()[startRow + step + step][1].getSymbol()
-      )
-    }
-  };
-
+  
   return {
     getBoard,
+    getCell,
     drawSymbol
   };
 })();
@@ -70,11 +48,11 @@ const board = (function() {
 function createPlayer(name, symbol) {
   this.name = name;
   this.symbol = symbol;
-
+  
   const getName = () => name;
-
+  
   const getSymbol = () => symbol;
-
+  
   return {
     getName,
     getSymbol
@@ -86,18 +64,20 @@ const controller = (function(board, player1, player2) {
   // Do they even need to be their own module?
   // Make this an IIFE (rename to Controller)
   let activePlayer = player1;
-
-  const switchPlayer = () => {
+  
+  function switchPlayer() {
     activePlayer = activePlayer === player1 ? player2 : player1;
   };
-
-  const getActivePlayer = () => activePlayer;
-
-  const startRound = () => {
+  
+  function getActivePlayer() {
+    return activePlayer;
+  }
+  
+  function startRound() {
     console.log(board.getBoard());
     console.log(`${getActivePlayer().getName()}'s turn.`);
-  };
-
+  }
+  
   const playRound = (row, column) => {
     if (board.drawSymbol(row, column, getActivePlayer().getSymbol())) {
       console.log(`Drew ${getActivePlayer().getSymbol()}'s symbol at (${column}, ${row})`);
@@ -106,8 +86,8 @@ const controller = (function(board, player1, player2) {
       startRound();
       return false;
     }
-
-    if (board.determineWinner(row, column)) {
+    
+    if (determineWinner(row, column)) {
       console.log(`${getActivePlayer().getName()} wins!`);
       return true;
     } else {
@@ -116,7 +96,62 @@ const controller = (function(board, player1, player2) {
       return false;
     }
   };
+  
+  function determineWinner() { // TODO: Refactor this mess
+    function checkRow(row) {
+      return (
+        board.getCell(row, 0).getSymbol() === board.getCell(row, 1).getSymbol() &&
+        board.getCell(row, 1).getSymbol() === board.getCell(row, 2).getSymbol()
+      );
+    }
+  
+    function checkColumn(column) {
+      return (
+        board.getCell(0, column).getSymbol() === board.getCell(1, column).getSymbol() && 
+        board.getCell(1, column).getSymbol() === board.getCell(2, column).getSymbol()
+      );
+    }
+  
+    function checkDiagonal(startRow, step) {
+      return (
+        board.getCell(startRow, 0).getSymbol() === board.getCell(startRow + step, 1).getSymbol() &&
+        board.getCell(startRow + step, 1).getSymbol() === board.getCell(startRow + step + step, 1).getSymbol()
+      )
+    }
 
+    if (checkRow(0)) {
+      return true;
+    }
+
+    if (checkRow(1)) {
+      return true;
+    }
+
+    if (checkRow(2)) {
+      return true;
+    }
+
+    if (checkColumn(0)) {
+      return true;
+    }
+
+    if (checkColumn(1)) {
+      return true;
+    }
+
+    if (checkColumn(2)) {
+      return true;
+    }
+
+    if (checkDiagonal(0, 1)) {
+      return true;
+    }
+
+    if (checkDiagonal(2, -1)) {
+      return true;
+    }
+  }
+  
   return {
     playRound,
     getActivePlayer
@@ -124,7 +159,9 @@ const controller = (function(board, player1, player2) {
 })(board, player1, player2);
 
 
+while (true) {
 
+}
 // controller = createController(Players, board)
 // controller.playRound() should return a boolean indicating the winner? if any?
 // have doWhile loop which represents the game
