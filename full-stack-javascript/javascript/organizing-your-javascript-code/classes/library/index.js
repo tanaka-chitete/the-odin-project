005532a7fd1d88763080b16437f70ea8cc6f6c1b
Tanaka Chitete <tanaka.chitete@gmail.com>
2024-08-17 14:56:53 +1000

    Use class instead of function

--- full-stack-javascript/javascript/organizing-your-javascript-code/classes/library/index.js
+++ full-stack-javascript/javascript/organizing-your-javascript-code/classes/library/index.js
@@ -1,70 +1,75 @@
-"use strict";
-
-function Book(title, author, numPages, read) {
-  if (!(this instanceof Book)) {
-    throw Error("Use the new operator to call this function");
+class Book {
+  constructor(title, author, numPages, read) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.read = read;
   }
-
-  this.title = title;
-  this.author = author;
-  this.numPages = numPages;
-  this.read = read;
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.numPages} pages, ${read ? "read" : "unread"}`;
+  
+  info() {
+    return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.read ? "read" : "unread"}`;
   }
 }
 
-const libraryObject = new Array();
+class Library {
+  constructor() {
+    this.books = [];
+  }
 
-function addBookToLibrary(addBookForm) {
-  const bookObject = new Book(
-    addBookForm.elements["title"].value, 
-    addBookForm.elements["author"].value, 
-    Number(addBookForm.elements["numPages"].value),
-    String(addBookForm.elements["read"].value) === "true"   
-  )
+  add(bookForm) {
+    const bookObject = new Book(
+      bookForm.elements["title"].value, 
+      bookForm.elements["author"].value, 
+      Number(bookForm.elements["numPages"].value),
+      String(bookForm.elements["read"].value) === "true"   
+    )
 
-  libraryObject.push(bookObject);
-  
-  addBookForm.reset();
-  addBookDialog.close();
+    this.books.push(bookObject);
 
-  const libraryElement = document.querySelector(".library");
-  
-  const bookElement = document.createElement("li");
-  bookElement.setAttribute("data-index", libraryElement.childElementCount);
-  bookElement.classList.add("library__book");
-  
-  const info = document.createElement("p");
-  info.classList.add("library__book-info");
-  info.textContent = `${bookObject.info()}`;
-  
-  const remove = document.createElement("button");
-  remove.classList.add("library__book-remove-book-button");
-  remove.textContent = "Remove";
-  remove.addEventListener("click", (event) => {
-    removeBookFromLibrary(event.target.parentElement);
-  })
-  
-  bookElement.append(info);
-  bookElement.append(remove);
-  
-  libraryElement.append(bookElement);
+    bookForm.reset();
+
+    const addBookDialog = document.querySelector(".add-book-dialog");
+    addBookDialog.close();
+
+    const libraryElement = document.querySelector(".library");
+
+    const bookElement = document.createElement("li");
+    bookElement.setAttribute("data-index", libraryElement.childElementCount);
+    bookElement.classList.add("library__book");
+    
+    const info = document.createElement("p");
+    info.classList.add("library__book-info");
+    info.textContent = `${bookObject.info()}`;
+
+    const remove = document.createElement("button");
+    remove.classList.add("library__book-remove-book-button");
+    remove.textContent = "Remove";
+    remove.addEventListener("click", (event) => {
+      this.remove(event.target.parentElement);
+    })
+
+    bookElement.append(info);
+    bookElement.append(remove);
+    
+    libraryElement.append(bookElement);
+  }
+
+  remove(bookElement) {
+    this.books.splice(Number(bookElement.dataset.index));
+    bookElement.remove();
+  }
 }
 
-function removeBookFromLibrary(bookElement) {
-  libraryObject.splice(Number(bookElement.dataset.index));
-  bookElement.remove();
-}
+const library = new Library();
 
 const addBookDialog = document.querySelector(".add-book-dialog");
 const addBookButton = document.querySelector(".add-book-button");
 
 addBookButton.addEventListener("click", () => addBookDialog.showModal());
 
-const addBookForm = document.querySelector(".add-book-dialog__form");
-addBookForm.addEventListener("submit", (event) => {
+const bookForm = document.querySelector(".add-book-dialog__form");
+bookForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  addBookToLibrary(event.target);  
+  library.add(event.target);  
 });
