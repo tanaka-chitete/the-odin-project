9beb2a765f894001b6a3f79693f73fb3c49b7956
Tanaka Chitete <tanaka.chitete@gmail.com>
2024-08-27 21:07:29 +1000

    Structure menu item

--- full-stack-javascript/javascript/organizing-your-javascript-code/restaurant-site/restaurant-site/src/index.js
+++ full-stack-javascript/javascript/organizing-your-javascript-code/restaurant-site/restaurant-site/src/index.js
@@ -3,8 +3,12 @@ import "./my-reset.css";
 import "./styles.css";
 
 import { displayHome } from "./home.js";
+import { displayMenu } from "./menu.js";
 
-displayHome();
+// displayHome();
 
 const homeButton = document.querySelector("#home");
 homeButton.addEventListener("click", () => displayHome());
+
+const menuButton = document.querySelector("#menu");
+menuButton.addEventListener("click", () => displayMenu());
---
+++ full-stack-javascript/javascript/organizing-your-javascript-code/restaurant-site/restaurant-site/src/menu.js
@@ -0,0 +1,14 @@
+const displayMenu = () => {
+  // const heroImage = document.createElement("div");
+  // heroImage.classList.add("hero-image");
+
+  // const main = document.querySelector("main");
+  // main.replaceChildren(heroImage);
+
+  const foodH1 = document.createElement("h1");
+  foodH1.classList.add("")
+  foodH1.textContent = "Food";
+  
+};
+
+export { displayMenu };
