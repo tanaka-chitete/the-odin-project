e98b72dc4d27106b08168d111f56e045b1aaf551
Tanaka Chitete <tanaka.chitete@gmail.com>
2024-09-27 22:16:07 +1000

    Complete User Story 3

--- full-stack-javascript/javascript/javascript-in-the-real-world/dynamic-user-interface-interactions/dropdown-menu/index.js
+++ full-stack-javascript/javascript/javascript-in-the-real-world/dynamic-user-interface-interactions/dropdown-menu/index.js
@@ -1,10 +1,12 @@
-const dropdownButton = document.querySelector(".dropdown-button");
-dropdownButton.addEventListener("click", () => {
-  const dropdownMenuItems = document.querySelector(".dropdown-menu-items");
-  console.log(dropdownMenuItems);
-  if (dropdownMenuItems.classList.contains("dropdown-menu-items_hidden")) {
-    dropdownMenuItems.classList.remove("dropdown-menu-items_hidden");
-  } else {
-    dropdownMenuItems.classList.add("dropdown-menu-items_hidden");
-  }
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach((dropdown) => {
+  const button = dropdown.querySelector(".dropdown-button");
+  button.addEventListener("click", () => {
+    const list = dropdown.querySelector(".dropdown-menu-items");
+    if (list.classList.contains("dropdown-menu-items_hidden")) {
+      list.classList.remove("dropdown-menu-items_hidden");
+    } else {
+      list.classList.add("dropdown-menu-items_hidden");
+    }
+  });
 });
