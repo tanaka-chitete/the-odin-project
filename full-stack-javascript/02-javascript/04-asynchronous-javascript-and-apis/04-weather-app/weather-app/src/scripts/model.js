b4bd4ccc104da73a4263fdb785aa24dec7b570c9
Tanaka Chitete <tanaka.chitete@gmail.com>
2025-02-05 20:41:58 +1100

    Cache response
    This will make testing much easier

--- full-stack-javascript/02-javascript/04-asynchronous-javascript-and-apis/04-weather-app/weather-app/src/scripts/model.js
+++ full-stack-javascript/02-javascript/04-asynchronous-javascript-and-apis/04-weather-app/weather-app/src/scripts/model.js
@@ -1,34 +1,40 @@
-import { format } from "date-fns";
+// import { format } from "date-fns";
 
-const TIME_WINDOW_IN_DAYS = 10;
-const DATE_FORMAT = "yyyy-MM-dd";
-const API_KEY = "ULLZAVP98LHVZBLKNFM5PZGCM";
+import { CACHED_FORECAST } from "./constants";
+
+// const TIME_WINDOW_IN_DAYS = 9;
+// const DATE_FORMAT = "yyyy-MM-dd";
+// const API_KEY = "ULLZAVP98LHVZBLKNFM5PZGCM";
 
 class Model {
-  async fetchWeatherData(location) {
-    const startDate = format(
-      new Date(new Date().setDate(new Date().getDate() - TIME_WINDOW_IN_DAYS)),
-      DATE_FORMAT
-    );
-    const endDate = format(new Date(), DATE_FORMAT);
+  fetchWeatherData = () => {
+    this.onWeatherDataFetched(CACHED_FORECAST["resolvedAddress"]);
+  };
 
-    try {
-      const response = await fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${startDate}/${endDate}?key=${API_KEY}`,
-        { mode: "cors" }
-      );
+  // async fetchWeatherData(location) {
+  //   const startDate = format(
+  //     new Date(new Date().setDate(new Date().getDate() - TIME_WINDOW_IN_DAYS)),
+  //     DATE_FORMAT
+  //   );
+  //   const endDate = format(new Date(), DATE_FORMAT);
 
-      if (!response.ok) {
-        throw new Error(await response.text());
-      }
+  //   try {
+  //     const response = await fetch(
+  //       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${startDate}/${endDate}?key=${API_KEY}`,
+  //       { mode: "cors" }
+  //     );
 
-      const weatherData = await response.json();
+  //     if (!response.ok) {
+  //       throw new Error(await response.text());
+  //     }
 
-      this.onWeatherDataFetched(weatherData["description"]);
-    } catch (error) {
-      this.onWeatherDataFetched(error);
-    }
-  }
+  //     const weatherData = await response.json();
+
+  //     this.onWeatherDataFetched(weatherData["resolvedAddress"]);
+  //   } catch (error) {
+  //     this.onWeatherDataFetched(error);
+  //   }
+  // }
 
   // fetchWeatherData = (location) => {
   //   const startDate = format(
@@ -50,7 +56,7 @@ class Model {
 
   //       return response.json();
   //     })
-  //     .then((data) => this.onWeatherDataFetched(data["description"]))
+  //     .then((data) => this.onWeatherDataFetched(data["resolvedAddress"]))
   //     .catch((error) => this.onWeatherDataFetched(error));
   // };
 
