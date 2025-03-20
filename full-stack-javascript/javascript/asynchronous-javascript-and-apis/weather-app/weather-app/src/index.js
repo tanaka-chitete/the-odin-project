6c1717fed0b53f3a2c42a6f626df01174b1bf964
Tanaka Chitete <tanaka.chitete@gmail.com>
2025-03-20 21:30:09 +1100

    Delete misplaced files

--- full-stack-javascript/javascript/asynchronous-javascript-and-apis/weather-app/weather-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles/main.css";
-
-import { Model } from "./scripts/model.js";
-import { View } from "./scripts/view.js";
-import { Controller } from "./scripts/controller.js";
-
-const controller = new Controller(new Model(), new View());
-
-controller.handleSearch("Paris");
--- full-stack-javascript/javascript/asynchronous-javascript-and-apis/weather-app/weather-app/webpack.prod.js
+++
@@ -1,6 +0,0 @@
-const { merge } = require("webpack-merge");
-
-const common = require("./webpack.common.js");
-const prod = { mode: "production" };
-
-module.exports = merge(common, prod);
