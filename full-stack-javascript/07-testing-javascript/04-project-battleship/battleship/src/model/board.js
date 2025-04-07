b2e7eb044531d04739d0276b696bdf2f866c9335
Tanaka Chitete <tanaka.chitete@gmail.com>
2025-04-07 20:50:13 +1000

    Convert Coordinates into a single object

--- full-stack-javascript/07-testing-javascript/04-project-battleship/battleship/src/model/board.js
+++ full-stack-javascript/07-testing-javascript/04-project-battleship/battleship/src/model/board.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { Coordinate } from "./coordinate";
+
 export class Board {
   #LENGTH_OF_BOARD = 10;
 
@@ -13,22 +15,88 @@ export class Board {
   }
 
   // TODO: Mark places using "x" and "o" for each player
-  place(ship, coordinates) {
-    if (
-      coordinates.startX < 0 ||
-      coordinates.startX >= this.#LENGTH_OF_BOARD ||
-      coordinates.startY < 0 ||
-      coordinates.startY >= this.#LENGTH_OF_BOARD ||
-      coordinates.endX < 0 ||
-      coordinates.endX >= this.#LENGTH_OF_BOARD ||
-      coordinates.endY < 0 ||
-      coordinates.endY >= this.#LENGTH_OF_BOARD
-    ) {
+  place(start, end) {
+    // TODO: Check for equality using utilities.equals()
+    // if (start === end) {
+    //   return false;
+    // }
+
+    if (!this.#isInBound(start) || !this.#isInBound(end)) {
       return false;
     }
 
-    // Case 1: ship at coordinates is invalid -> true
-    // if ()
-    // Case 2: ship at coordinates is valid -> false
+    if (!this.#isInline(start, end)) {
+      return false;
+    }
+
+    // TODO: Check if coordinates would place a ship of length >5
+    if (!this.#isOfSize(start, end)) {
+      return false;
+    }
+
+    const path = this.#makePath(start, end);
+    if (!this.#isFree(path)) {
+      return false;
+    }
+
+    path.forEach(
+      (coordinate) => (this.#board[coordinate.y][coordinate.x] = "x")
+    );
+
+    return true; // TODO: Return the ship
+  }
+
+  #isInline(start, end) {
+    return start.x === end.x || start.y === end.y;
+  }
+
+  #isInBound(coordinate) {
+    return (
+      coordinate.x >= 0 &&
+      coordinate.x < this.#LENGTH_OF_BOARD &&
+      coordinate.y >= 0 &&
+      coordinate.y < this.#LENGTH_OF_BOARD
+    );
+  }
+
+  #isOfSize(start, end) {
+    return (
+      (start.x === end.x && Math.abs(end.y - start.y) <= 5) ||
+      (start.y === end.y && Math.abs(end.x - start.x) <= 5)
+    );
+  }
+
+  #isFree(path) {
+    return path.every(
+      (coordinate) => this.#board[coordinate.y][coordinate.x] === ""
+    );
+  }
+
+  #makePath(start, end) {
+    const path = [];
+
+    if (start.x === end.x) {
+      if (start.y > end.y) {
+        // Coordinates need to be swapped to avoid confusion
+        [start, end] = [end, start];
+
+        // Path needs to be made using coordinates on shared vertical
+        for (let row = start.y; row <= end.y; row++) {
+          path.push(new Coordinate(start.x, row));
+        }
+      }
+    } else {
+      if (start.x > end.x) {
+        // Coordinates need to be swapped to avoid confusion
+        [start, end] = [end, start];
+
+        // Path needs to be made using coordinates on shared horizontal
+        for (let column = start.x; column <= end.x; column++) {
+          path.push(new Coordinate(column, start.y));
+        }
+      }
+    }
+
+    return [start, ...path, end];
   }
 }
--- full-stack-javascript/07-testing-javascript/04-project-battleship/battleship/src/model/board.spec.js
+++ full-stack-javascript/07-testing-javascript/04-project-battleship/battleship/src/model/board.spec.js
@@ -1,39 +1,160 @@
 "use strict";
 
 import { Board } from "./board";
-import { Coordinates } from "./coordinates";
-import { Ship } from "./ship";
+import { Coordinate } from "./coordinate";
 
 describe("Board", () => {
   describe("place()", () => {
-    it("does not place a ship at an under-dimensions (negative) coordinate", () => {
-      expect(new Board().place(new Ship(2), new Coordinates(-1, 0, 0, 0))).toBe(
-        false
-      );
-      expect(new Board().place(new Ship(2), new Coordinates(0, -1, 0, 0))).toBe(
-        false
-      );
-      expect(new Board().place(new Ship(2), new Coordinates(0, 0, -1, 0))).toBe(
-        false
-      );
-      expect(new Board().place(new Ship(2), new Coordinates(0, 0, 0, -1))).toBe(
-        false
-      );
+    it("does not place a ship at an under-dimensions (negative) position", () => {
+      expect(
+        new Board().place(new Coordinate(-1, 0), new Coordinate(0, 0))
+      ).toBe(false);
+
+      // expect(
+      //   new Board().place(new Ship(2), [
+      //     new Coordinate(0, -1),
+      //     new Coordinate(0, 0),
+      //   ])
+      // ).toBe(false);
+
+      // expect(
+      //   new Board().place(new Ship(2), [
+      //     new Coordinate(0, 0),
+      //     new Coordinate(-1, 0),
+      //   ])
+      // ).toBe(false);
+
+      // expect(
+      //   new Board().place(new Ship(2), [
+      //     new Coordinate(0, 0),
+      //     new Coordinate(0, -1),
+      //   ])
+      // ).toBe(false);
     });
 
-    it("does not place a ship at an over-dimensions (>=10) coordinate", () => {
-      expect(new Board().place(new Ship(2), new Coordinates(10, 0, 0, 0))).toBe(
-        false
-      );
-      expect(new Board().place(new Ship(2), new Coordinates(0, 10, 0, 0))).toBe(
-        false
-      );
-      expect(new Board().place(new Ship(2), new Coordinates(0, 0, 10, 0))).toBe(
-        false
-      );
-      expect(new Board().place(new Ship(2), new Coordinates(0, 0, 0, 10))).toBe(
-        false
-      );
-    });
+    // it("only places the ship at linear coordinates", () => {
+    //   // Doesn't place diagonally
+    //   // Doesn't place scattered
+    //   // Doesn't place in a box
+    // })
+
+    // it("does not place a ship at an over-dimensions (>=10) position", () => {
+    //   expect(
+    //     new Board().place(new Ship(2), [
+    //       new Coordinate(10, 0),
+    //       new Coordinate(0, 0),
+    //     ])
+    //   ).toBe(false);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(0, 10),
+    //   //     new Coordinate(0, 0),
+    //   //   ])
+    //   // ).toBe(false);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(0, 0),
+    //   //     new Coordinate(10, 0),
+    //   //   ])
+    //   // ).toBe(false);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(0, 0),
+    //   //     new Coordinate(0, 10),
+    //   //   ])
+    //   // ).toBe(false);
+    // });
+
+    // it("places ship at in-bound position", () => {
+    //   expect(
+    //     new Board().place(new Ship(2), [
+    //       new Coordinate(0, 0),
+    //       new Coordinate(0, 1),
+    //     ])
+    //   ).toBe(true);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(0, 0),
+    //   //     new Coordinate(1, 0),
+    //   //   ])
+    //   // ).toBe(true);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(0, 9),
+    //   //     new Coordinate(0, 8),
+    //   //   ])
+    //   // ).toBe(true);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(0, 9),
+    //   //     new Coordinate(1, 9),
+    //   //   ])
+    //   // ).toBe(true);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(9, 9),
+    //   //     new Coordinate(8, 9),
+    //   //   ])
+    //   // ).toBe(true);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(9, 9),
+    //   //     new Coordinate(9, 8),
+    //   //   ])
+    //   // ).toBe(true);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(9, 0),
+    //   //     new Coordinate(9, 1),
+    //   //   ])
+    //   // ).toBe(true);
+
+    //   // expect(
+    //   //   new Board().place(new Ship(2), [
+    //   //     new Coordinate(9, 0),
+    //   //     new Coordinate(8, 0),
+    //   //   ])
+    //   // ).toBe(true);
+    // });
+
+    // it("does not place ship at currently-occupied position", () => {
+    //   const board = new Board();
+
+    //   expect(
+    //     board.place(new Ship(2), [new Coordinate(0, 0), new Coordinate(0, 1)])
+    //   ).toBe(true);
+
+    //   // expect(
+    //   //   board.place(new Ship(2), [new Coordinate(0, 0), new Coordinate(0, 1)])
+    //   // ).toBe(false);
+    // });
+
+    // it("does not place ship at non-inline coordinates", () => {
+    //   expect(
+    //     new Board().place(new Ship(3), [
+    //       new Coordinate(0, 0),
+    //       new Coordinate(1, 0),
+    //       new Coordinate(1, 1),
+    //     ])
+    //   ).toBe(false);
+    // });
+
+    // it("does not place ship at non-consecutive coordinates", () => {
+    //   expect(
+    //     new Board().place(new Ship(2), [
+    //       new Coordinate(0, 0),
+    //       new Coordinate(2, 0),
+    //     ])
+    //   ).toBe(false);
+    // });
   });
 });
