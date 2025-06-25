2aceaf4591cde0fc325de607e8e0743203587f44
Tanaka Chitete <tanaka.chitete@gmail.com>
2025-06-25 20:59:44 +1000

    restarting battleship

--- full-stack-javascript/07-testing-javascript/04-project-battleship/battleship/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import "./styles/main.css";
-
-import { Controller } from "./scripts/controller/controller";
-import { Model } from "./scripts/model/model";
-import { View } from "./scripts/view/view";
-
-const app = new Controller(new Model(), new View());
